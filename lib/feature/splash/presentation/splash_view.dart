b390e226a1aca3ca17154a60c07907c48a78b2e0
Mohamed Samir <mohamed.samir.ibrahim98@gmail.com>
2025-11-10 19:39:24 +0200

    edit in splash

--- lib/feature/splash/presentation/splash_view.dart
+++ lib/feature/splash/presentation/splash_view.dart
@@ -1,10 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
   static const String routeName = 'splash';
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () =>
+        Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Scaffold(),),),);
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        height: MediaQuery
+            .of(context)
+            .size
+            .height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/Onboarding.png'),
+            fit: BoxFit.cover, // or BoxFit.fill, BoxFit.contain, etc.
+          ),
+        ),
+        child: Center(
+          child: Image.asset('assets/splash.png', width: MediaQuery
+              .of(context)
+              .size
+              .width, height: MediaQuery
+              .of(context)
+              .size
+              .height,),
+        ),
+      ),
+    );
   }
 }
